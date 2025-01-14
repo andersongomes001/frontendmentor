e51ed3c6ea20267cd8096e7053e9775b86d2a4d1
Anderson Gomes <comprasgomes@hotmail.com>
2025-01-14 11:08:04 -0300

    Update app.js

--- faq-accordion-main/assets/js/app.js
+++ faq-accordion-main/assets/js/app.js
@@ -4,10 +4,10 @@ window.onload = () => {
       console.log(document.querySelectorAll(".response")[index]);
       if (new String(item.src).indexOf("minus") == -1) {
         item.src = "assets/images/icon-minus.svg";
-        document.querySelectorAll(".response")[index].display = "inline";
+        document.querySelectorAll(".response")[index].style.display = "inline";
       } else {
         item.src = "assets/images/icon-plus.svg";
-        document.querySelectorAll(".response")[index].display = "none";
+        document.querySelectorAll(".response")[index].style.display = "none";
       }
     });
   });
