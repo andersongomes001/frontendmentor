ef04a115845df3259cfe3969c038b6ec44709712
Anderson <comprasgomes@hotmail.com>
2025-01-14 08:33:28 -0300

    solved faq-accordion-main challenge

---
+++ faq-accordion-main/assets/js/app.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  document.querySelectorAll(".dropdown-icon").forEach((item, index) => {
+    item.addEventListener("click", () => {
+      console.log(document.querySelectorAll(".response")[index]);
+      if (new String(item.src).indexOf("minus") == -1) {
+        item.src = "assets/images/icon-minus.svg";
+        document.querySelectorAll(".response")[index].display = "inline";
+      } else {
+        item.src = "assets/images/icon-plus.svg";
+        document.querySelectorAll(".response")[index].display = "none";
+      }
+    });
+  });
+};
